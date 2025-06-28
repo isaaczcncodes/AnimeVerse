1d5b8238f646454b8bf63ee24719a9cf0bfb838d
Isaac José Ferreira da Silva <isaaczcncodes@gmail.com>
2025-06-28 12:14:09 -0300

    Update AnimeController.java

--- backend/src/main/java/com/animeverse/controller/AnimeController.java
+++ backend/src/main/java/com/animeverse/controller/AnimeController.java
@@ -1,1 +1,13 @@
+package com.animeverse.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AnimeController {
+
+    @GetMapping("/api/animes")
+    public String getAnimes() {
+        return "AnimeVerse API ON";
+    }
+}
