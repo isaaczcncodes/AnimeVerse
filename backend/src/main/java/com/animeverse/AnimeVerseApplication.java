1a15bcd806e2852e5c28cb26f47f3d081ac58478
isaaczcncodes <isaaczcncodes@gmail.com>
2025-06-29 21:40:39 -0300

    Revisão geral no projeto, reorganização de pastas e adiocionando arquivos do backend

--- backend/src/main/java/com/animeverse/AnimeVerseApplication.java
+++ backend/src/main/java/com/animeverse/AnimeVerseApplication.java
@@ -1,4 +1,4 @@
-package com.animeverse;
+package com.animeverse.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ backend/src/test/java/com/animeverse/AnimeVerseApplicationTests.java
@@ -0,0 +1,1 @@
+package com.animeverse.backend;
